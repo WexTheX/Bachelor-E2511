9bbec1495ad4950572d57fbe8cf22f4426b60beb
WexTheX <wexthex@gmail.com>
2025-05-18 22:36:07 +0200

    renamed realtime to livedetection

--- User_interface.py
+++ User_interface.py
@@ -2,11 +2,11 @@ import streamlit as st
 from main import main
 import os
 import asyncio
-from realtime import RT_main, get_predictions
+from live_detection import RT_main, get_predictions
 import time
 import threading
 from streamlit_autorefresh import st_autorefresh
-from realtime import shutdown_event
+from live_detection import shutdown_event
 import pandas as pd
 import numpy as np
 # args for main()
